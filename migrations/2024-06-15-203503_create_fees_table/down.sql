accb8263bde0c6e2ca14413b87554e205b657e76
Joey Kraut <joey@renegade.fi>
2024-06-15 13:50:27 -0700

    migrations: Setup migrations and create `fees` & `last_indexed` tables

---
+++ migrations/2024-06-15-203503_create_fees_table/down.sql
@@ -0,0 +1,4 @@
+-- Drop the fees table and indexes
+DROP TABLE IF EXISTS fees;
+DROP INDEX IF EXISTS idx_fees_mint;
+DROP INDEX IF EXISTS idx_fees_amount;
---
+++ src/schema.rs
@@ -0,0 +1,25 @@
+// @generated automatically by Diesel CLI.
+
+diesel::table! {
+    fees (id) {
+        id -> Int4,
+        tx_hash -> Text,
+        mint -> Text,
+        amount -> Numeric,
+        blinder -> Numeric,
+        receiver -> Text,
+        redeemed -> Bool,
+    }
+}
+
+diesel::table! {
+    indexing_metadata (key) {
+        key -> Text,
+        value -> Text,
+    }
+}
+
+diesel::allow_tables_to_appear_in_same_query!(
+    fees,
+    indexing_metadata,
+);
