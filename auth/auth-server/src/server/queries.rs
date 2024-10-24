f338e650d96f62c21fe1a846da927e62e78997c6
Joey Kraut <joey@renegade.fi>
2024-10-24 13:17:55 -0700

    auth-server: Add local api key cache

--- auth/auth-server/src/server/queries.rs
+++ auth/auth-server/src/server/queries.rs
@@ -16,6 +16,12 @@ impl Server {
 
     /// Get the API key entry for a given key
     pub async fn get_api_key_entry(&self, api_key: Uuid) -> Result<ApiKey, AuthServerError> {
+        // Check the cache first
+        if let Some(key) = self.get_cached_api_secret(api_key).await {
+            return Ok(key);
+        }
+
+        // Fetch the key from the database
         let mut conn = self.get_db_conn().await?;
         let result = api_keys::table
             .filter(api_keys::id.eq(api_key))
@@ -23,35 +29,54 @@ impl Server {
             .load::<ApiKey>(&mut conn)
             .await
             .map_err(AuthServerError::db)?;
+        drop(conn); // Drop the connection to release the mutable borrow on `self`
 
-        if result.is_empty() {
-            Err(AuthServerError::unauthorized("API key not found"))
+        let key = if result.is_empty() {
+            return Err(AuthServerError::unauthorized("API key not found"));
         } else {
-            Ok(result[0].clone())
+            result[0].clone()
+        };
+
+        // Cache the key and return
+        self.cache_api_key(key.clone()).await;
+        if !key.is_active {
+            return Err(AuthServerError::ApiKeyInactive);
         }
+
+        Ok(key)
     }
 
     // --- Setters --- //
 
     /// Add a new API key to the database
     pub async fn add_key_query(&self, new_key: NewApiKey) -> Result<(), AuthServerError> {
+        // Write to the database
         let mut conn = self.get_db_conn().await?;
         diesel::insert_into(api_keys::table)
             .values(&new_key)
             .execute(&mut conn)
             .await
-            .map_err(AuthServerError::db)
-            .map(|_| ())
+            .map_err(AuthServerError::db)?;
+        drop(conn); // Drop the connection to release the mutable borrow on `self`
+
+        // Cache the key
+        self.cache_api_key(new_key.into()).await;
+        Ok(())
     }
 
     /// Expire an existing API key
     pub async fn expire_key_query(&self, key_id: Uuid) -> Result<(), AuthServerError> {
+        // Update the database
         let mut conn = self.get_db_conn().await?;
         diesel::update(api_keys::table.filter(api_keys::id.eq(key_id)))
             .set(api_keys::is_active.eq(false))
             .execute(&mut conn)
             .await
-            .map_err(AuthServerError::db)
-            .map(|_| ())
+            .map_err(AuthServerError::db)?;
+        drop(conn); // Drop the connection to release the mutable borrow on `self`
+
+        // Remove the key from the cache
+        self.mark_cached_key_expired(key_id).await;
+        Ok(())
     }
 }
