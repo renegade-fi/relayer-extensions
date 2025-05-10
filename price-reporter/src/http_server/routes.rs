3cb031f0e8deb1a2df3a570655abcb75ec03a899
Sehyun Chung ✌︎ <41171808+sehyunc@users.noreply.github.com>
2025-05-10 13:43:21 -0700

    price-reporter: use multiple remaps

--- price-reporter/src/http_server/routes.rs
+++ price-reporter/src/http_server/routes.rs
@@ -4,9 +4,8 @@ use async_trait::async_trait;
 use futures_util::StreamExt;
 use hyper::{body::to_bytes, Body, Request, Response, StatusCode};
 use renegade_api::auth::validate_expiring_auth;
-use renegade_common::types::{exchange::Exchange, hmac::HmacKey, Price};
+use renegade_common::types::{chain::Chain, exchange::Exchange, hmac::HmacKey, Price};
 use renegade_config::setup_token_remaps;
-use renegade_darkpool_client::constants::Chain;
 use renegade_price_reporter::worker::ExchangeConnectionsConfig;
 use renegade_util::err_str;
 
