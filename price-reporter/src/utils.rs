3185d8d5869e9019061329b5ad43bde239c8cb44
Sehyun Chung ✌︎ <41171808+sehyunc@users.noreply.github.com>
2025-05-14 18:02:55 -0700

    price-reporter: init default tickers

--- price-reporter/src/utils.rs
+++ price-reporter/src/utils.rs
@@ -1,5 +1,6 @@
 //! Miscellaneous utility types and helper functions.
 
+use std::collections::HashSet;
 use std::pin::Pin;
 use std::task::{Context, Poll};
 use std::{collections::HashMap, env, str::FromStr, sync::Arc};
@@ -274,12 +275,16 @@ pub fn get_subscribed_topics(subscriptions: &PriceStreamMap) -> Vec<String> {
     subscriptions.keys().map(get_price_topic_str).collect_vec()
 }
 
-/// Get all tokens from the token map filtering out given tokens
-pub fn get_all_tokens_filtered(filtered_tokens: &[&str]) -> Vec<Token> {
-    get_all_tokens()
-        .into_iter()
-        .filter(|t| !filtered_tokens.contains(&t.get_ticker().unwrap().as_str()))
-        .collect_vec()
+/// Get all distinct tickers from configured token remap(s)
+pub fn get_all_distinct_tickers() -> Vec<String> {
+    let mut tickers = HashSet::new();
+    get_all_tokens().into_iter().for_each(|t| {
+        if let Some(ticker) = t.get_ticker() {
+            tickers.insert(ticker);
+        }
+    });
+
+    tickers.into_iter().collect_vec()
 }
 
 /// Given an address, search through the token remaps to find the token and
