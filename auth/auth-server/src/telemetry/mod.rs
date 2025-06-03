b3d198d9adaf816d2b3e9f053f9aa001389dd013
Joey Kraut <joey@renegade.fi>
2025-06-02 17:14:14 -0700

    auth-server: telemetry: Enable OTLP and open span for each request

--- auth/auth-server/src/telemetry/mod.rs
+++ auth/auth-server/src/telemetry/mod.rs
@@ -18,18 +18,20 @@ pub mod sources;
 pub const QUOTE_FILL_RATIO_IGNORE_THRESHOLD: u128 = 100_000 * 10u128.pow(6u32); // $100,000 of USDC
 /// The prefix for metrics
 const METRICS_PREFIX: &str = "auth-server";
+/// The default OTLP collector endpoint
+const DEFAULT_OTLP_COLLECTOR_ENDPOINT: &str = "http://localhost:4317";
 
 /// Configure telemetry from the command line arguments
 pub(crate) fn configure_telemtry_from_args(args: &Cli) -> Result<(), AuthServerError> {
     let metrics_config =
         MetricsConfig { metrics_prefix: METRICS_PREFIX.to_string(), ..Default::default() };
     configure_telemetry_with_metrics_config(
-        args.datadog_enabled, // datadog_enabled
-        false,                // otlp_enabled
-        args.metrics_enabled, // metrics_enabled
-        "".to_string(),       // collector_endpoint
-        &args.statsd_host,    // statsd_host
-        args.statsd_port,     // statsd_port
+        args.datadog_enabled,                        // datadog_enabled
+        args.otlp_enabled,                           // otlp_enabled
+        args.metrics_enabled,                        // metrics_enabled
+        DEFAULT_OTLP_COLLECTOR_ENDPOINT.to_string(), // collector_endpoint
+        &args.statsd_host,                           // statsd_host
+        args.statsd_port,                            // statsd_port
         Some(metrics_config),
     )
     .map_err(AuthServerError::setup)
