ad865ff6213dfca9659ad2617e1a2d52f3f63050
Andrew Kirillov <20803092+akirillo@users.noreply.github.com>
2025-08-01 10:57:29 -0700

    funds-manager: execution_client: use ExecutableQuote for Lifi execution (#321)

--- funds-manager/funds-manager-server/src/execution_client/venues/lifi.rs
+++ funds-manager/funds-manager-server/src/execution_client/venues/lifi.rs
@@ -10,7 +10,10 @@ use serde::Deserialize;
 
 use crate::execution_client::{
     error::ExecutionClientError,
-    venues::{ExecutableQuote, ExecutionQuote, QuoteExecutionData, SupportedExecutionVenue},
+    venues::{
+        quote::{ExecutableQuote, ExecutionQuote, QuoteExecutionData},
+        SupportedExecutionVenue,
+    },
 };
 
 /// Transaction request details from LiFi API
@@ -71,7 +74,66 @@ pub struct LifiQuote {
     tool: String,
 }
 
+impl LifiQuote {
+    /// Get the token being sold
+    fn get_sell_token(&self, chain: Chain) -> Token {
+        Token::from_addr_on_chain(&self.action.from_token.address, chain)
+    }
+
+    /// Get the token being bought
+    fn get_buy_token(&self, chain: Chain) -> Token {
+        Token::from_addr_on_chain(&self.action.to_token.address, chain)
+    }
+
+    /// Get the amount of tokens being sold
+    fn get_sell_amount(&self) -> Result<U256, ExecutionClientError> {
+        U256::from_str_radix(&self.estimate.from_amount, 10)
+            .map_err(ExecutionClientError::quote_conversion)
+    }
+
+    /// Get the amount of tokens being bought
+    fn get_buy_amount(&self) -> Result<U256, ExecutionClientError> {
+        U256::from_str_radix(&self.estimate.to_amount, 10)
+            .map_err(ExecutionClientError::quote_conversion)
+    }
+
+    /// Get the address of the swap contract that will be called
+    fn get_to_address(&self) -> Result<Address, ExecutionClientError> {
+        self.transaction_request.to.parse().map_err(ExecutionClientError::quote_conversion)
+    }
+
+    /// Get the address of the submitting address
+    fn get_from_address(&self) -> Result<Address, ExecutionClientError> {
+        self.action.from_address.parse().map_err(ExecutionClientError::quote_conversion)
+    }
+
+    /// Get the value of the tx; should be zero
+    fn get_value(&self) -> Result<U256, ExecutionClientError> {
+        U256::from_str_radix(self.transaction_request.value.trim_start_matches("0x"), 16)
+            .map_err(ExecutionClientError::quote_conversion)
+    }
+
+    /// Get the calldata for the swap
+    fn get_data(&self) -> Result<Bytes, ExecutionClientError> {
+        hex::decode(self.transaction_request.data.trim_start_matches("0x"))
+            .map_err(ExecutionClientError::quote_conversion)
+            .map(Bytes::from)
+    }
+
+    /// Get the gas limit for the swap
+    fn get_gas_limit(&self) -> Result<U256, ExecutionClientError> {
+        U256::from_str_radix(self.transaction_request.gas_limit.trim_start_matches("0x"), 16)
+            .map_err(ExecutionClientError::quote_conversion)
+    }
+
+    /// Get the tool (venue) providing the route
+    fn get_tool(&self) -> String {
+        self.tool.clone()
+    }
+}
+
 /// Lifi-specific quote execution data
+#[derive(Debug, Clone)]
 pub struct LifiQuoteExecutionData {
     /// The swap contract address
     pub to: Address,
@@ -93,14 +155,10 @@ impl ExecutableQuote {
         lifi_quote: LifiQuote,
         chain: Chain,
     ) -> Result<Self, ExecutionClientError> {
-        let sell_token = Token::from_addr_on_chain(&lifi_quote.action.from_token.address, chain);
-        let buy_token = Token::from_addr_on_chain(&lifi_quote.action.to_token.address, chain);
-
-        let sell_amount = U256::from_str_radix(&lifi_quote.estimate.from_amount, 10)
-            .map_err(ExecutionClientError::quote_conversion)?;
-
-        let buy_amount = U256::from_str_radix(&lifi_quote.estimate.to_amount, 10)
-            .map_err(ExecutionClientError::quote_conversion)?;
+        let sell_token = lifi_quote.get_sell_token(chain);
+        let buy_token = lifi_quote.get_buy_token(chain);
+        let sell_amount = lifi_quote.get_sell_amount()?;
+        let buy_amount = lifi_quote.get_buy_amount()?;
 
         let quote = ExecutionQuote {
             sell_token,
@@ -111,33 +169,12 @@ impl ExecutableQuote {
             chain,
         };
 
-        let to = lifi_quote
-            .transaction_request
-            .to
-            .parse()
-            .map_err(ExecutionClientError::quote_conversion)?;
-
-        let from = lifi_quote
-            .action
-            .from_address
-            .parse()
-            .map_err(ExecutionClientError::quote_conversion)?;
-
-        let value =
-            U256::from_str_radix(lifi_quote.transaction_request.value.trim_start_matches("0x"), 16)
-                .map_err(ExecutionClientError::quote_conversion)?;
-
-        let data = hex::decode(lifi_quote.transaction_request.data.trim_start_matches("0x"))
-            .map_err(ExecutionClientError::quote_conversion)?
-            .into();
-
-        let gas_limit = U256::from_str_radix(
-            lifi_quote.transaction_request.gas_limit.trim_start_matches("0x"),
-            16,
-        )
-        .map_err(ExecutionClientError::quote_conversion)?;
-
-        let tool = lifi_quote.tool;
+        let to = lifi_quote.get_to_address()?;
+        let from = lifi_quote.get_from_address()?;
+        let value = lifi_quote.get_value()?;
+        let data = lifi_quote.get_data()?;
+        let gas_limit = lifi_quote.get_gas_limit()?;
+        let tool = lifi_quote.get_tool();
 
         let execution_data = LifiQuoteExecutionData { to, from, value, data, gas_limit, tool };
 
--- funds-manager/funds-manager-server/src/execution_client/venues/mod.rs
+++ funds-manager/funds-manager-server/src/execution_client/venues/mod.rs
@@ -1,11 +1,9 @@
 //! Venue-specific logic for getting quotes and executing swaps
 
-use alloy_primitives::U256;
-use renegade_common::types::{chain::Chain, token::Token};
-
-use crate::execution_client::venues::lifi::LifiQuoteExecutionData;
+use std::fmt::Display;
 
 pub mod lifi;
+pub mod quote;
 
 /// An enum used to specify supported execution venues
 pub enum SupportedExecutionVenue {
@@ -15,37 +13,11 @@ pub enum SupportedExecutionVenue {
     Cowswap,
 }
 
-/// The basic information included in an execution quote,
-/// agnostic of the venue that provided the quote
-pub struct ExecutionQuote {
-    /// The token being sold
-    pub sell_token: Token,
-    /// The token being bought
-    pub buy_token: Token,
-    /// The amount of the token being sold, in atoms
-    pub sell_amount: U256,
-    /// The quoted amount of the token being bought, in atoms
-    pub buy_amount: U256,
-    /// The venue that provided the quote
-    pub venue: SupportedExecutionVenue,
-    /// The chain for which the quote was generated
-    pub chain: Chain,
-}
-
-/// An enum wrapping the venue-specific auxiliary data needed to execute a quote
-pub enum QuoteExecutionData {
-    /// Lifi-specific quote execution data
-    Lifi(LifiQuoteExecutionData),
-    /// Cowswap-specific quote execution data
-    // TODO: Implement Cowswap quote execution data
-    Cowswap(),
-}
-
-/// An executable quote, which includes the basic quote information
-/// along with any auxiliary data needed to execute the quote
-pub struct ExecutableQuote {
-    /// The quote
-    pub quote: ExecutionQuote,
-    /// The venue-specific auxiliary data needed to execute the quote
-    pub execution_data: QuoteExecutionData,
+impl Display for SupportedExecutionVenue {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            SupportedExecutionVenue::Lifi => write!(f, "Lifi"),
+            SupportedExecutionVenue::Cowswap => write!(f, "Cowswap"),
+        }
+    }
 }
