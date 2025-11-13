c21eb1fbf51c8cd96eecf850f1a1ee8d21586a39
Andrew Kirillov <20803092+akirillo@users.noreply.github.com>
2025-11-13 10:39:56 -0800

    indexer: darkpool_client: init darkpool client & impl new balance parsing (#483)

--- indexer/darkpool-indexer/src/api/handlers/sqs.rs
+++ indexer/darkpool-indexer/src/api/handlers/sqs.rs
@@ -1,110 +1,85 @@
 //! Handler logic SQS messages polled by the darkpool indexer
 
-use alloy::{primitives::TxHash, providers::Provider};
 use aws_sdk_sqs::types::Message;
 use darkpool_indexer_api::types::sqs::{MasterViewSeedMessage, RecoveryIdMessage, SqsMessage};
-use renegade_constants::Scalar;
 
 use crate::{
     api::handlers::error::HandlerError, indexer::Indexer, state_transitions::types::StateTransition,
 };
 
-// -----------------------------
-// | Top-Level Message Handler |
-// -----------------------------
+impl Indexer {
+    // -----------------------------
+    // | Top-Level Message Handler |
+    // -----------------------------
 
-/// Handle a message polled from SQS, parsing it into the API message type and
-/// applying the appropriate handler logic
-pub async fn handle_sqs_message(
-    message: Message,
-    indexer: &Indexer,
-    sqs_queue_url: &str,
-) -> Result<(), HandlerError> {
-    if let Some(body) = message.body() {
-        let message: SqsMessage = serde_json::from_str(body)?;
-        match message {
-            SqsMessage::RegisterMasterViewSeed(message) => {
-                handle_master_view_seed_message(message, indexer).await?;
-            },
-            SqsMessage::RegisterRecoveryId(message) => {
-                handle_recovery_id_message(message, indexer).await?;
-            },
-            SqsMessage::NullifierSpend(_) => {
-                todo!()
-            },
+    /// Handle a message polled from SQS, parsing it into the API message type
+    /// and applying the appropriate handler logic
+    pub async fn handle_sqs_message(
+        &self,
+        message: Message,
+        sqs_queue_url: &str,
+    ) -> Result<(), HandlerError> {
+        if let Some(body) = message.body() {
+            let message: SqsMessage = serde_json::from_str(body)?;
+            match message {
+                SqsMessage::RegisterMasterViewSeed(message) => {
+                    self.handle_master_view_seed_message(message).await?;
+                },
+                SqsMessage::RegisterRecoveryId(message) => {
+                    self.handle_recovery_id_message(message).await?;
+                },
+                SqsMessage::NullifierSpend(_) => {
+                    todo!()
+                },
+            }
         }
+
+        if let Some(receipt_handle) = message.receipt_handle() {
+            self.sqs_client
+                .delete_message()
+                .queue_url(sqs_queue_url)
+                .receipt_handle(receipt_handle)
+                .send()
+                .await?;
+        }
+
+        Ok(())
     }
 
-    if let Some(receipt_handle) = message.receipt_handle() {
-        indexer
-            .sqs_client
-            .delete_message()
-            .queue_url(sqs_queue_url)
-            .receipt_handle(receipt_handle)
-            .send()
-            .await?;
+    // ------------
+    // | Handlers |
+    // ------------
+
+    // === Master View Seed Message Handler ===
+
+    /// Handle a SQS message representing the registration of a new master view
+    /// seed
+    pub async fn handle_master_view_seed_message(
+        &self,
+        message: MasterViewSeedMessage,
+    ) -> Result<(), HandlerError> {
+        let state_transition = StateTransition::RegisterMasterViewSeed(message);
+
+        self.state_applicator.apply_state_transition(state_transition).await?;
+
+        // TODO: Kick off backfill
+
+        Ok(())
     }
 
-    Ok(())
-}
+    // === Recovery ID Message Handler ===
 
-// ------------
-// | Handlers |
-// ------------
+    /// Handle an SQS message representing the registration of a new recovery ID
+    pub async fn handle_recovery_id_message(
+        &self,
+        message: RecoveryIdMessage,
+    ) -> Result<(), HandlerError> {
+        let RecoveryIdMessage { recovery_id, tx_hash } = message;
+        let state_transition =
+            self.darkpool_client.get_state_transition_for_recovery_id(recovery_id, tx_hash).await?;
 
-// === Master View Seed Message Handler ===
+        self.state_applicator.apply_state_transition(state_transition).await?;
 
-/// Handle a SQS message representing the registration of a new master view seed
-pub async fn handle_master_view_seed_message(
-    message: MasterViewSeedMessage,
-    indexer: &Indexer,
-) -> Result<(), HandlerError> {
-    let state_transition = StateTransition::RegisterMasterViewSeed(message);
-
-    indexer.state_applicator.apply_state_transition(state_transition).await?;
-
-    // TODO: Kick off backfill
-
-    Ok(())
-}
-
-// === Recovery ID Message Handler ===
-
-/// Handle an SQS message representing the registration of a new recovery ID
-pub async fn handle_recovery_id_message(
-    message: RecoveryIdMessage,
-    indexer: &Indexer,
-) -> Result<(), HandlerError> {
-    let RecoveryIdMessage { recovery_id, tx_hash } = message;
-    let state_transition =
-        get_state_transition_for_recovery_id(recovery_id, tx_hash, indexer).await?;
-
-    indexer.state_applicator.apply_state_transition(state_transition).await?;
-
-    Ok(())
-}
-
-// -----------
-// | Helpers |
-// -----------
-
-/// Get the state transition associated with the given recovery ID's
-/// registration
-async fn get_state_transition_for_recovery_id(
-    _recovery_id: Scalar,
-    tx_hash: TxHash,
-    indexer: &Indexer,
-) -> Result<StateTransition, HandlerError> {
-    let registration_tx = indexer
-        .ws_provider
-        .get_transaction_receipt(tx_hash)
-        .await
-        .map_err(HandlerError::rpc)?
-        .ok_or(HandlerError::rpc(format!("Transaction receipt not found for tx {tx_hash:#x}")))?;
-
-    let _block_number = registration_tx
-        .block_number
-        .ok_or(HandlerError::rpc("Block number not found in tx {tx_hash:#x} receipt"))?;
-
-    todo!()
+        Ok(())
+    }
 }
--- indexer/darkpool-indexer/src/indexer/mod.rs
+++ indexer/darkpool-indexer/src/indexer/mod.rs
@@ -1,16 +1,42 @@
 //! Defines the indexer struct, a dependency injection container which stores
 //! handles to shared resources
 
-use alloy::providers::{DynProvider, Provider, ProviderBuilder, WsConnect};
+use alloy::{
+    hex,
+    primitives::Address,
+    providers::{Provider, ProviderBuilder, WsConnect},
+};
 use aws_config::Region;
 use aws_sdk_sqs::Client as SqsClient;
+use renegade_common::types::chain::Chain;
+use renegade_solidity_abi::v2::IDarkpoolV2::IDarkpoolV2Instance;
 
 use crate::{
-    cli::Cli, db::client::DbClient, indexer::error::IndexerError,
+    cli::Cli, darkpool_client::DarkpoolClient, db::client::DbClient, indexer::error::IndexerError,
     state_transitions::StateApplicator,
 };
 
 pub mod error;
+
+// -------------
+// | Constants |
+// -------------
+
+/// The address of the darkpool contract deployed on Arbitrum One
+const ARBITRUM_ONE_DARKPOOL_ADDRESS: Address =
+    Address::new(hex!("0x0000000000000000000000000000000000000000"));
+/// The address of the darkpool contract deployed on Arbitrum Sepolia
+const ARBITRUM_SEPOLIA_DARKPOOL_ADDRESS: Address =
+    Address::new(hex!("0x0000000000000000000000000000000000000000"));
+/// The address of the darkpool contract deployed on Base mainnet
+const BASE_MAINNET_DARKPOOL_ADDRESS: Address =
+    Address::new(hex!("0x0000000000000000000000000000000000000000"));
+/// The address of the darkpool contract deployed on Base Sepolia
+const BASE_SEPOLIA_DARKPOOL_ADDRESS: Address =
+    Address::new(hex!("0x0000000000000000000000000000000000000000"));
+/// The address of the darkpool contract deployed on devnet
+const DEVNET_DARKPOOL_ADDRESS: Address =
+    Address::new(hex!("0x0000000000000000000000000000000000000000"));
 
 /// The indexer struct. Stores handles to shared resources.
 #[derive(Clone)]
@@ -19,8 +45,8 @@ pub struct Indexer {
     pub state_applicator: StateApplicator,
     /// The AWS SQS client
     pub sqs_client: SqsClient,
-    /// The WebSocket Ethereum RPC provider
-    pub ws_provider: DynProvider,
+    /// The darkpool client
+    pub darkpool_client: DarkpoolClient,
 }
 
 impl Indexer {
@@ -36,13 +62,24 @@ impl Indexer {
 
         let sqs_client = SqsClient::new(&config);
 
-        // Set up the WebSocket RPC provider
+        // Set up the WebSocket RPC provider & darkpool client
         let ws = WsConnect::new(&cli.ws_rpc_url);
         let ws_provider =
             ProviderBuilder::default().connect_ws(ws).await.map_err(IndexerError::rpc)?.erased();
 
+        let darkpool_address = match cli.chain {
+            Chain::ArbitrumOne => ARBITRUM_ONE_DARKPOOL_ADDRESS,
+            Chain::ArbitrumSepolia => ARBITRUM_SEPOLIA_DARKPOOL_ADDRESS,
+            Chain::BaseMainnet => BASE_MAINNET_DARKPOOL_ADDRESS,
+            Chain::BaseSepolia => BASE_SEPOLIA_DARKPOOL_ADDRESS,
+            Chain::Devnet => DEVNET_DARKPOOL_ADDRESS,
+        };
+
+        let darkpool = IDarkpoolV2Instance::new(darkpool_address, ws_provider);
+        let darkpool_client = DarkpoolClient::new(darkpool);
+
         // TODO: Parse remaining CLI arguments
 
-        Ok(Self { state_applicator, sqs_client, ws_provider })
+        Ok(Self { state_applicator, sqs_client, darkpool_client })
     }
 }
