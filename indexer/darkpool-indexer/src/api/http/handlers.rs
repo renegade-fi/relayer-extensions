58629303441bd27d90834345b6f74e3fe3866600
Andrew Kirillov <20803092+akirillo@users.noreply.github.com>
2025-11-21 11:42:35 -0800

    indexer: api: http: add 'get user state' endpoint (#512)

--- indexer/darkpool-indexer/src/api/http/handlers.rs
+++ indexer/darkpool-indexer/src/api/http/handlers.rs
@@ -2,11 +2,21 @@
 
 use std::sync::Arc;
 
-use darkpool_indexer_api::types::http::BackfillRequest;
+use darkpool_indexer_api::types::http::{ApiStateObject, BackfillRequest, GetUserStateResponse};
+use diesel_async::{AsyncConnection, scoped_futures::ScopedFutureExt};
 use tracing::error;
+use uuid::Uuid;
 use warp::{http::StatusCode, reject::Rejection, reply::Reply};
 
-use crate::indexer::Indexer;
+use crate::{
+    api::http::error::ApiError,
+    db::{client::DbClient, error::DbError},
+    indexer::{Indexer, error::IndexerError},
+};
+
+// --------------------
+// | Backfill Handler |
+// --------------------
 
 /// Handle a request to backfill a user's state
 pub async fn handle_backfill_request(
@@ -24,4 +34,52 @@ pub async fn handle_backfill_request(
     });
 
     Ok(warp::reply::with_status("OK", StatusCode::OK))
+}
+
+// --------------------------
+// | Get User State Handler |
+// --------------------------
+
+/// Handle a request to get a user's state
+pub async fn handle_get_user_state_request(
+    account_id: Uuid,
+    indexer: Arc<Indexer>,
+) -> Result<impl Reply, Rejection> {
+    let active_state_objects = get_all_active_user_state_objects(account_id, &indexer.db_client)
+        .await
+        .map_err(ApiError::internal_server_error)?;
+
+    Ok(warp::reply::json(&GetUserStateResponse { active_state_objects }))
+}
+
+/// Get all of a user's active state objects
+async fn get_all_active_user_state_objects(
+    account_id: Uuid,
+    db_client: &DbClient,
+) -> Result<Vec<ApiStateObject>, IndexerError> {
+    let mut conn = db_client.get_db_conn().await?;
+
+    let (balances, intents, public_intents) = conn
+        .transaction(move |conn| {
+            async move {
+                let balances = db_client.get_account_active_balances(account_id, conn).await?;
+
+                let intents = db_client.get_account_active_intents(account_id, conn).await?;
+
+                let public_intents =
+                    db_client.get_account_active_public_intents(account_id, conn).await?;
+
+                Ok::<_, DbError>((balances, intents, public_intents))
+            }
+            .scope_boxed()
+        })
+        .await?;
+
+    let api_balances = balances.into_iter().map(Into::into);
+    let api_intents = intents.into_iter().map(Into::into);
+    let api_public_intents = public_intents.into_iter().map(Into::into);
+
+    let active_state_objects = api_balances.chain(api_intents).chain(api_public_intents).collect();
+
+    Ok(active_state_objects)
 }
--- indexer/darkpool-indexer/src/api/http/routes.rs
+++ indexer/darkpool-indexer/src/api/http/routes.rs
@@ -3,14 +3,15 @@
 use std::sync::Arc;
 
 use darkpool_indexer_api::{
-    routes::{BACKFILL_PATH, HEALTHCHECK_PATH},
+    routes::{BACKFILL_PATH, GET_USER_STATE_PATH, HEALTHCHECK_PATH},
     types::http::BackfillRequest,
 };
+use uuid::Uuid;
 use warp::{Filter, Rejection, Reply, filters::BoxedFilter, http::StatusCode};
 
 use crate::{
     api::http::{
-        handlers::handle_backfill_request,
+        handlers::{handle_backfill_request, handle_get_user_state_request},
         middleware::{handle_rejection, identity, with_hmac_auth, with_indexer, with_json_body},
     },
     indexer::Indexer,
@@ -28,12 +29,24 @@ pub fn http_routes(indexer: Arc<Indexer>) -> BoxedFilter<(impl Reply,)> {
         .and(with_hmac_auth(indexer.clone()))
         .map(with_json_body::<BackfillRequest>)
         .and_then(identity)
-        .and(with_indexer(indexer))
+        .and(with_indexer(indexer.clone()))
         .and_then(handle_backfill_request);
+
+    let user_state_route = warp::get()
+        .and(warp::path(GET_USER_STATE_PATH))
+        .and(warp::path::param::<Uuid>())
+        .and(with_hmac_auth(indexer.clone()))
+        .and(with_indexer(indexer))
+        .and_then(|account_id, _, indexer| handle_get_user_state_request(account_id, indexer));
 
     let not_found_fallback = warp::any().and_then(|| async {
         Ok::<_, Rejection>(warp::reply::with_status("Not Found", StatusCode::NOT_FOUND))
     });
 
-    healthcheck_route.or(backfill_route).or(not_found_fallback).recover(handle_rejection).boxed()
+    healthcheck_route
+        .or(backfill_route)
+        .or(user_state_route)
+        .or(not_found_fallback)
+        .recover(handle_rejection)
+        .boxed()
 }
