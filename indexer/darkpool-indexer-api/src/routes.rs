7870777dee5eadd104ea657bc9bd21ec0abe8752
Andrew Kirillov <andrew@kirillov.org>
2025-11-21 11:41:34 -0800

    indexer: api: http: add 'get user state' endpoint

--- indexer/darkpool-indexer-api/src/routes.rs
+++ indexer/darkpool-indexer-api/src/routes.rs
@@ -5,3 +5,6 @@ pub const HEALTHCHECK_PATH: &str = "healthcheck";
 
 /// The path for the backfill route
 pub const BACKFILL_PATH: &str = "backfill";
+
+/// The path for the get user state route
+pub const GET_USER_STATE_PATH: &str = "user-state";
