2f75f88fdc6ecca298b92ca4b8ce0ce5e26c73e2
Andrew Kirillov <andrew@kirillov.org>
2025-11-26 17:11:37 -0800

    indexer: integration: add anvil, integration, & prod docker images

---
+++ indexer/darkpool-indexer/integration/anvil/deploy_contracts.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -Eeuo pipefail
+
+# Constants
+DEPLOY_SCRIPT="script/v2/DeployDev.s.sol:DeployDevScript"
+ANVIL_RPC_URL="http://localhost:8545"
+ANVIL_PKEY="0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
+
+
+# Spawn a local Anvil node which will snapshot its state upon exit
+anvil \
+    --disable-code-size-limit \
+    --state /anvil-state.json \
+    &
+
+ANVIL_PID=$!
+
+# Add a small sleep to ensure the Anvil node ready
+sleep 1
+
+# Run the deployment script
+forge script \
+    $DEPLOY_SCRIPT \
+    --ffi \
+    --rpc-url $ANVIL_RPC_URL \
+    --private-key $ANVIL_PKEY \
+    --broadcast \
+    --disable-code-size-limit
+
+# Copy deployments file to the expected location
+cp deployments.json /deployments.json
+
+# Send a termination signal to the Anvil node to snapshot its state
+kill -TERM $ANVIL_PID
+
+# Wait for the Anvil node to exit
+wait $ANVIL_PID
