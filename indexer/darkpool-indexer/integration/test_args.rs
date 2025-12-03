8c384213fa9207b651c3fe2778f16e3fac503ee3
Andrew Kirillov <20803092+akirillo@users.noreply.github.com>
2025-12-03 15:15:50 -0800

    indexer: integration: setup test context (#530)

--- indexer/darkpool-indexer/integration/test_args.rs
+++ indexer/darkpool-indexer/integration/test_args.rs
@@ -1,13 +1,13 @@
 //! Defines the arguments passed to every test
 
-use std::{path::PathBuf, str::FromStr};
+use std::{cell::OnceCell, path::PathBuf, str::FromStr, sync::Arc};
 
 use alloy::{
     primitives::{Address, U256},
-    providers::{DynProvider, Provider, ext::AnvilApi},
+    providers::{DynProvider, Provider},
     signers::local::PrivateKeySigner,
 };
-use darkpool_indexer::{indexer::Indexer, types::MasterViewSeed};
+use darkpool_indexer::{db::test_utils::cleanup_test_db, indexer::Indexer, types::MasterViewSeed};
 use eyre::Result;
 use postgresql_embedded::PostgreSQL;
 use renegade_circuit_types::csprng::PoseidonCSPRNG;
@@ -16,8 +16,8 @@ use renegade_solidity_abi::v2::IDarkpoolV2::IDarkpoolV2Instance;
 use crate::{
     CliArgs,
     utils::setup::{
-        BASE_TOKEN_DEPLOYMENT_KEY, PERMIT2_DEPLOYMENT_KEY, build_test_indexer,
-        gen_test_master_view_seed, read_deployment, register_test_master_view_seed, run_blocking,
+        BASE_TOKEN_DEPLOYMENT_KEY, PERMIT2_DEPLOYMENT_KEY, gen_test_master_view_seed,
+        read_deployment,
     },
 };
 
@@ -25,31 +25,51 @@ use crate::{
 #[derive(Clone)]
 pub struct TestArgs {
     /// The path to the contract deployments file
-    deployments: PathBuf,
-    /// The indexer instance to test
-    indexer: Indexer,
-    /// The local PostgreSQL instance to use for testing
-    postgres: PostgreSQL,
-    /// The ID of the Anvil snapshot from which to run each test
-    anvil_snapshot_id: U256,
+    pub deployments: PathBuf,
+    /// The WS RPC URL of the Anvil node to use for testing
+    pub anvil_ws_url: String,
+    /// The test context, constructed during test harness setup
+    pub test_context: OnceCell<TestContext>,
     /// The first test account's master view seed, which will be pre-allocated
     /// into the indexer
-    party0_master_view_seed: MasterViewSeed,
+    pub party0_master_view_seed: MasterViewSeed,
     /// The first test account's private key
-    party0_signer: PrivateKeySigner,
+    pub party0_signer: PrivateKeySigner,
+}
+
+/// A container for resources used by all integration tests
+/// which are constructed during test harness setup
+#[derive(Clone)]
+pub struct TestContext {
+    /// The indexer instance to test
+    pub indexer: Indexer,
+    /// The local PostgreSQL instance to use for testing
+    pub postgres: Arc<PostgreSQL>,
+    /// The ID of the Anvil snapshot from which to run each test
+    pub anvil_snapshot_id: U256,
 }
 
 impl TestArgs {
+    // --- Test Context Helpers --- //
+
+    /// Get a reference to the test context, expecting it to be set
+    pub fn expect_test_context(&self) -> &TestContext {
+        self.test_context.get().unwrap()
+    }
+
     // --- RPC Client Helpers --- //
+
     /// Get the chain ID of the Anvil node
     pub async fn chain_id(&self) -> Result<u64> {
-        let chain_id = self.indexer.darkpool_client.provider().get_chain_id().await?;
+        let test_context = self.expect_test_context();
+        let chain_id = test_context.indexer.darkpool_client.provider().get_chain_id().await?;
         Ok(chain_id)
     }
 
     /// Get the darkpool instance
     pub fn darkpool_instance(&self) -> IDarkpoolV2Instance<DynProvider> {
-        self.indexer.darkpool_client.darkpool.clone()
+        let test_context = self.expect_test_context();
+        test_context.indexer.darkpool_client.darkpool.clone()
     }
 
     // --- Test Account Helpers --- //
@@ -74,6 +94,7 @@ impl TestArgs {
     pub fn next_party0_recovery_stream(&mut self) -> PoseidonCSPRNG {
         let recovery_stream_seed =
             self.party0_master_view_seed.recovery_seed_csprng.next().unwrap();
+
         PoseidonCSPRNG::new(recovery_stream_seed)
     }
 
@@ -81,7 +102,8 @@ impl TestArgs {
 
     /// Get the darkpool contract address
     pub fn darkpool_address(&self) -> Address {
-        self.indexer.darkpool_client.darkpool_address()
+        let test_context = self.expect_test_context();
+        test_context.indexer.darkpool_client.darkpool_address()
     }
 
     /// Get the Permit2 contract address
@@ -93,32 +115,30 @@ impl TestArgs {
     pub fn base_token_address(&self) -> Result<Address> {
         read_deployment(BASE_TOKEN_DEPLOYMENT_KEY, &self.deployments)
     }
+
+    // --- Teardown --- //
+
+    /// Clean up the test DB
+    pub async fn cleanup_test_db(&self) -> Result<()> {
+        let test_context = self.expect_test_context();
+        cleanup_test_db(&test_context.postgres).await?;
+        Ok(())
+    }
 }
 
 impl From<CliArgs> for TestArgs {
     fn from(value: CliArgs) -> Self {
-        run_blocking(async {
-            let party0_signer = PrivateKeySigner::from_str(&value.pkey)?;
+        let party0_signer = PrivateKeySigner::from_str(&value.pkey).unwrap();
 
-            let (indexer, postgres) =
-                build_test_indexer(&value.anvil_ws_url, party0_signer.clone(), &value.deployments)
-                    .await?;
+        let party0_master_view_seed = gen_test_master_view_seed(&party0_signer);
 
-            let anvil_snapshot_id = indexer.darkpool_client.provider().anvil_snapshot().await?;
-
-            let party0_master_view_seed = gen_test_master_view_seed(&party0_signer);
-            register_test_master_view_seed(&indexer, &party0_master_view_seed).await?;
-
-            let test_args = Self {
-                deployments: value.deployments,
-                indexer,
-                postgres,
-                anvil_snapshot_id,
-                party0_master_view_seed,
-                party0_signer,
-            };
-
-            Ok::<_, eyre::Report>(test_args)
-        })
+        Self {
+            deployments: value.deployments,
+            anvil_ws_url: value.anvil_ws_url,
+            // The test context will be constructed during test harness setup
+            test_context: OnceCell::new(),
+            party0_master_view_seed,
+            party0_signer,
+        }
     }
 }
